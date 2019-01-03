7adedb713fe5ad02d1aec18bd218d0e5dd30e15a
yirleison <yppalomeque@gmail.com>
2019-01-02 21:38:09 -0500

    Se  organiza el de la tabla de multiplicar de forma modular

--- 04-bases-node/app.js
+++ 04-bases-node/app.js
@@ -1,8 +1,10 @@
-
+let { crearArchivo } = require('./multiplicar/multiplicar');
 let consola = console.log;
 let base = 2;
 
 
+crearArchivo(base)
+.then(archivo => consola('Archivo creado'))
+.catch(err => consola(err));
 
-  crearArchivo(base)
   
--- 04-bases-node/multiplicar/multiplicar.js
+++ 04-bases-node/multiplicar/multiplicar.js
@@ -3,18 +3,27 @@ let data = '';
 
 let consola = console.log;
 
-crearArchivo= (base) =>{
+let crearArchivo = (base) => {
 
-    return new Promise ((resolve, reject)=>{
+    return new Promise ((resolve, reject) => {
+        if( !Number(base) ){
+            reject('No es un numero');
+            return;
+        }
 
-        for (let i = 1; i < 1 <= 10; i++) {
+        for (let i = 1; i <= 10; i++) {
             data+=`${ base } * ${ i } = ${ base * i }\n`;
           }
           
           fs.writeFile(`tablas/tabla-${ base }.txt`, data, (err) => {
-              if (err) reject(err)
+              if (err) 
+                reject(err)
               else
                 resolve(`tabla-${ base }.txt`)
             });
     });
+}
+
+module.exports = {
+    crearArchivo
 }
