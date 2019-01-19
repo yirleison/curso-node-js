7cf7052e02947b3af4ad5ef0eea9fc58905ac6ee
yirleison <yipmore@bancolombia.com.co>
2019-01-19 17:12:21 -0500

    Se termina la primera face de la aplicacion y se sube el proyecto a heroku

--- 07-web-server/server.js
+++ 07-web-server/server.js
@@ -1,20 +1,29 @@
 const express = require('express')
+const hbs = require('hbs');
+require ('./hbs/helpers');
 const app = express();
 let consola = console.log;
+
+const port = process.env.POT || 3000;
 
 //midleware => sirve para ejecutar acciones antes de que se ejecute una petición solicitada desde la web u otra parte
 app.use ( express.static(__dirname + '/public') )
 //Express HBS engine
+hbs.registerPartials(__dirname + '/views/parciales');
 app.set('view engine', 'hbs');
 
 app.get('/', (req, res) => {
    
     res.render('home',{
-        nombre: 'Yirleison',
-        anio: new Date().getFullYear()
+        nombre: 'Yirleison'
     });
 })
+
+app.get('/about', (req, res) => {
+   
+   res.render('about');
+})
  
-app.listen(3000,() => {
-    consola('Escuchando   el servidor en el puerto 3000');
+app.listen(port,() => {
+    consola(`Escuchando   el servidor en el puerto ${3000}`);
 })
